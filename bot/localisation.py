484456a71b154b100181b5151fab8ca0ce990fd6
BEN <112600905+ALiENxBEN@users.noreply.github.com>
2024-05-03 17:13:07 +0530

    Update localisation.py

--- bot/localisation.py
+++ bot/localisation.py
@@ -43,7 +43,7 @@ class Localisation:
     
     HELP_MESSAGE = get_config(
         "STRINGS_HELP_MESSAGE",
-        "Hi, I am Video Compressor Bot \n\n1. Send me your telegram big video file \n2. Reply to the file with: `/compress 50` \n\nSupport Group: @Hunters_Discussion"
+        "Hi, I am Video Compressor Bot \n\n1. Send me your telegram big video file \n2. Reply to the file with: `/compress 50` \n\nSupport Group: @About_Ben"
     )
     WRONG_MESSAGE = get_config(
         "STRINGS_WRONG_MESSAGE",
